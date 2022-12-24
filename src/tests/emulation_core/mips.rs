398a5f9bb98c9ef3040e9b76ba28f2138e2be7b9
Jerrett Longworth <jerrett7@gmail.com>
2022-12-23 19:27:41 -0500

    tests: reformat add_register_to_itself

--- src/tests/emulation_core/mips.rs
+++ src/tests/emulation_core/mips.rs
@@ -4,21 +4,23 @@ use crate::emulation_core::mips::registers::RegisterType;
 
 #[allow(clippy::unusual_byte_groupings)]
 #[test]
-fn add_register_to_itself() -> Result<(), String> {
+fn add_register_to_itself() {
     let mut datapath = MipsDatapath::default();
 
-    // T1 = T1 + T1
-    //                       R-type  T1    T1    T1  (shamt)  ADD
+    // $t1 = $t1 + $t1
+    //                       R-type  t1    t1    t1  (shamt)  ADD
     let instruction: u32 = 0b000000_01001_01001_01001_00000_100000;
-    datapath.memory.store_word(0, instruction)?;
+    datapath
+        .memory
+        .store_word(0, instruction)
+        .expect("Failed to store instruction.");
 
-    // Assume the T1 register has the value 5.
+    // Assume the register $t1 has the value 5.
     datapath.registers[RegisterType::T1] = 5;
 
     datapath.execute_instruction();
 
     // After the operation is finished, the register should be 10.
-    (datapath.registers[RegisterType::T1] == 10)
-        .then_some(())
-        .ok_or_else(|| String::from("Unexpected value in register."))
+    assert_eq!(datapath.registers[RegisterType::T1], 10);
+}
 }
