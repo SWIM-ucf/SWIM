36071f9d7d6028cc4bcb008dd581b8e528d34223
jerrettl <jerrettl@users.noreply.github.com>
2022-12-24 00:37:20 +0000

    deploy: dabcecac02020619c22b1a7bf864588bb89d0479

--- swim-734fd15f79b7c596.js
+++ swim-734fd15f79b7c596.js
@@ -527,7 +527,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('swim-b7cd90f78a2d409c_bg.wasm', import.meta.url);
+        input = new URL('swim-734fd15f79b7c596_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
