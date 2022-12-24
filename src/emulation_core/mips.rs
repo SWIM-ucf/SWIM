2222c3dd5acd9eefd7a4c9fb6f78f583234e3e5a
Jerrett Longworth <jerrett7@gmail.com>
2022-12-23 19:26:46 -0500

    emulation_core: Expand documentation

--- src/emulation_core/mips.rs
+++ src/emulation_core/mips.rs
@@ -1,4 +1,5 @@
-//! An implementation of the MIPS64 ISA.
+//! All facets of this project's implementation of the MIPS64 ISA, including
+//! the datapath, control signals, registers, and memory.
 
 pub mod control_signals;
 pub mod datapath;
