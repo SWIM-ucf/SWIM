d0f51171e2089eeaa3f1c2df9d30b84d71cb8d7f
jerrettl <jerrettl@users.noreply.github.com>
2023-01-05 17:27:52 +0000

    deploy: f232593ffa20eaa34087bea4e2412fbb6a41a0d7

--- swim-25493e9ff894d288.js
+++ swim-25493e9ff894d288.js
@@ -720,7 +720,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('swim-f40c92f85d377442_bg.wasm', import.meta.url);
+        input = new URL('swim-25493e9ff894d288_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
