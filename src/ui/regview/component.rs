89241dbc168a4f1f7e3f7f40f3e1591d50166166
huynguyen2019 <huynguyenhome2416@gmail.com>
2023-01-15 16:17:55 -0500

    making table

--- src/ui/regview/component.rs
+++ src/ui/regview/component.rs
@@ -5,8 +5,20 @@ pub fn regview() -> Html {
     html! {
         <>
             <div>
-                <div style="width: 365px; height: 734px; border: 1px solid black; background-color: lightgray; float: right;">
-                </div>
+                <table style="width: 365px; height: 734px; border: 1px solid black; background-color: white; float: right;">
+                    <tr style="border: 1px solid black;">
+                        <th style="border: 1px solid black;">{"GP"}</th>
+                        <th style="border: 1px solid black;">{"FP"}</th>
+                    </tr>
+                    <tr style="border: 1px solid black;">
+                        <td style="border: 1px solid black;"></td>
+                        <td style="border: 1px solid black;"></td>
+                    </tr>
+                    <tr style="border: 1px solid black;">
+                        <td style="border: 1px solid black;"></td>
+                        <td style="border: 1px solid black;"></td>
+                    </tr>
+                </table>
             </div>
         </>
     }
