f61ac462cac73674cc6c9ec00645201df48facb9
huynguyen2019 <60188307+huynguyen2019@users.noreply.github.com>
2023-01-26 11:55:57 -0500

    Console and regview branch (#104)
    * made some boxes
    
    basic stuff
    
    * aside
    
    * add float to move it right
    
    * making table
    
    * dynamic size
    
    * using better units
    
    * Create tabs.rs
    
    * screen fitting
    
    * resolve merge conflict
    
    * resolve merge conflict 2
    
    * progress on regview
    
    * changed according to clippy
    
    * update
    
    * reformatted - clippy

---
+++ src/ui/console/component.rs
@@ -0,0 +1,19 @@
+use yew::prelude::*;
+
+#[derive(Properties, PartialEq)]
+pub struct Consoleprops {
+    pub parsermsg: String,
+}
+
+#[function_component(Console)]
+pub fn console(props: &Consoleprops) -> Html {
+    html! {
+        <>
+            // <div style="width: 80.4%; height: 17vh; border: 1px solid black;"></div>
+            <div style="width: 80.4%; height: 17vh; border: 2px solid black; background-color: #b9cceb; color: #000000;">
+                <p>{ props.parsermsg.clone() }</p>
+                //<p>{ ">" }</p>
+            </div>
+        </>
+    }
+}
