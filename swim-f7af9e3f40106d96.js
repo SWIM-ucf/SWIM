c1a4ccacf96cf202afa158a8120e3c8495c7f6b4
kevinacahalan <kevinacahalan@users.noreply.github.com>
2023-01-30 16:14:17 +0000

    deploy: c074a00f28ab2147042b8d75ac9c046f67037049

--- swim-f7af9e3f40106d96.js
+++ swim-f7af9e3f40106d96.js
@@ -736,7 +736,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('swim-7d67f473cd8a6f45_bg.wasm', import.meta.url);
+        input = new URL('swim-f7af9e3f40106d96_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
