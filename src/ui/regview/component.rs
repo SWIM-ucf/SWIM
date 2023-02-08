ae9f2517f642f780240f37b9eac713484f754459
huynguyen2019 <huynguyenhome2416@gmail.com>
2023-02-08 15:37:51 -0500

    switch views done...

--- src/ui/regview/component.rs
+++ src/ui/regview/component.rs
@@ -10,6 +10,11 @@ pub struct Regviewprops {
     pub fp: [u64; 32],
 }
 
+#[derive(Properties, PartialEq)]
+pub struct Viewswitch {
+    pub switch_view: bool,
+}
+
 //Convert register to html through iterator
 pub fn gen_reg_html(gp: GpRegisters) -> Html {
     gp.into_iter()
@@ -51,23 +56,27 @@ pub fn regview(props: &Regviewprops) -> Html {
     let on_switch_clicked = {
         let switch_flag = switch_flag.clone();
         use_callback(
-            move |_, _| {
-                switch_flag.set(!(*switch_flag));
+            move |_, switch_flag| {
+                if **switch_flag{
+                    switch_flag.set(false);
+                } else {
+                    switch_flag.set(true);
+                }
             }, 
-            (),
+            switch_flag,
         )
     };
     log!("This is ", *switch_flag);
     html! {
         <>
-        <button onclick={on_switch_clicked}>{"Switch view"}</button>
+        <button onclick={on_switch_clicked} style="float: right">{"Switch view"}</button>
             <div>
                 <table style="width: 19.2%; height: 97vh; border: 1px solid black; background-color: white; float: right;" content="width=device-width; initial-scale=1.0">
                     <tr style="border: 1px solid black;">
                         <th style="border: 1px solid black;">{"Register Name"}</th>
                         <th style="border: 1px solid black;">{"Data"}</th>
                     </tr>
-                    if (*switch_flag.clone()) == true {
+                    if (*switch_flag) == true {
                         {gen_reg_html(props.gp)}
                     } else {
                         {fp_reg(props.fp)}
