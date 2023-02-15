b32c495973ded9c47a681e52e1bc99552dfa935d
Jerrett Longworth <jerrett7@gmail.com>
2023-02-15 14:28:09 -0500

    line_info: Add lines for start

--- src/emulation_core/mips/line_info.rs
+++ src/emulation_core/mips/line_info.rs
@@ -23,6 +23,34 @@ impl VisualDatapath for MipsDatapath {
 
     fn visual_line_to_data(&self, variable: &str) -> LineInformation {
         match variable {
+            "pc" => LineInformation {
+                title: String::from("Program Counter"),
+                description: String::from("The address of the instruction to be executed next."),
+                value: self.registers.pc,
+                bits: 32,
+            },
+
+            "register_write_data" => LineInformation {
+                title: String::from("Register Write Data"),
+                description: String::from("Data that will be written to a general-purpose register."),
+                value: self.state.register_write_data,
+                bits: 64,
+            },
+
+            "rt" => LineInformation {
+                title: String::from("Instruction [20-16]"),
+                description: String::from("The rt field. Contains the second register to be read for an R-type instruction."),
+                value: self.state.rt as u64,
+                bits: 5,
+            },
+
+            "coprocessor.fs" => LineInformation {
+                title: String::from("Instruction [15-11]"),
+                description: String::from("The fs field. Contains the first register to be read in a floating-point instruction."),
+                value: self.coprocessor.state.fs as u64,
+                bits: 5,
+            },
+            
             _ => LineInformation {
                 title: String::from("[Title]"),
                 description: String::from("[Description]"),
