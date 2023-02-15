155601cfe5312efbce34e6e48d51378d99be1a5f
Jerrett Longworth <jerrett7@gmail.com>
2023-02-15 14:25:15 -0500

    visual datapath: Create populate_popup_information()

--- src/ui/visual_datapath/utils.rs
+++ src/ui/visual_datapath/utils.rs
@@ -4,6 +4,8 @@ use gloo::utils::document;
 use wasm_bindgen::JsCast;
 use web_sys::{Element, HtmlCollection, HtmlElement, HtmlObjectElement};
 
+use crate::emulation_core::{datapath::VisualDatapath, mips::datapath::MipsDatapath};
+
 use super::consts::*;
 
 /// Returns an [`HtmlObjectElement`] corresponding to the `<object>` element in HTML.
@@ -70,4 +72,42 @@ where
 
         function(&element);
     }
+}
+
+/// Set the data contained in the popup.
+///
+/// Parameters:
+/// - `datapath`: A reference to the datapath that information will be pulled from.
+/// - `variable`: The "variable" attribute of the line in the diagram that will have information.
+pub fn populate_popup_information(datapath: &MipsDatapath, variable: &str) {
+    let popup = get_popup_element();
+
+    let title = popup.query_selector(".title").unwrap().unwrap();
+    let description = popup.query_selector(".description").unwrap().unwrap();
+    let bits = popup.query_selector(".data .code").unwrap().unwrap();
+    let meaning = popup.query_selector(".meaning").unwrap().unwrap();
+
+    let information = datapath.visual_line_to_data(variable);
+
+    title.set_text_content(Some(&information.title));
+    description.set_text_content(Some(&information.description));
+    bits.set_text_content(Some(&u64_to_bits(information.value, information.bits)));
+    meaning.set_text_content(Some(&u64::to_string(&information.value)));
+}
+
+/// Convert an integer value to a string, limited to `bits` number of bits.
+///
+/// If `bits` is less than 64, the lower `bits` number of bits in `value` will be used.
+pub fn u64_to_bits(mut value: u64, bits: u64) -> String {
+    let mut output = String::new();
+
+    for _ in 0..bits {
+        let bit = (value % 2) as u32;
+        output.push(char::from_digit(bit, 10).unwrap_or_default());
+        value /= 2;
+    }
+
+    output = output.chars().rev().collect::<String>();
+
+    output
 }
