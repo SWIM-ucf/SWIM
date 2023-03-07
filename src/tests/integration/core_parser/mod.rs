f0e1947a36943be778d0caae94995ca9623a5157
Jerrett Longworth <jerrett7@gmail.com>
2023-03-07 02:05:28 -0500

    tests/integration: Add double arithmetic tests

--- src/tests/integration/core_parser/mod.rs
+++ src/tests/integration/core_parser/mod.rs
@@ -7,6 +7,7 @@ use crate::parser::parser_assembler_main::parser;
 pub mod basic_immediate;
 pub mod basic_operations;
 pub mod conditions;
+pub mod double_arithmetic;
 
 #[test]
 fn add_register_plus_itself() -> Result<(), String> {
