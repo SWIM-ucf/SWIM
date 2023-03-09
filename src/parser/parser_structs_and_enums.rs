224611a7a9d6314d7d14eb27292b4b877f489fff
Jerrett Longworth <jerrett7@gmail.com>
2023-03-08 19:02:26 -0500

    Adjust formatting

--- src/parser/parser_structs_and_enums.rs
+++ src/parser/parser_structs_and_enums.rs
@@ -111,7 +111,7 @@ pub mod instruction_tokenization {
 
     impl fmt::Display for ErrorType {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            write!(f, "{:?}", self)
+            write!(f, "{self:?}")
         }
     }
 
