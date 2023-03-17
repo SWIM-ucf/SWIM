fa441460a7032a65d51db48c2b61d3b1ee78515b
b0x-Cub3d <b0x-Cub3d@users.noreply.github.com>
2023-03-17 16:59:56 +0000

    deploy: 406fd83e4f8bb57d5246469c4a3451ddfda59402

--- swim-714edc0d02da9ee5.js
+++ swim-714edc0d02da9ee5.js
@@ -1165,7 +1165,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('swim-80b16bfbdc95a96_bg.wasm', import.meta.url);
+        input = new URL('swim-714edc0d02da9ee5_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
