aa9e9f659d05dcc6f2fc9623c896acd8252229bf
Jerrett Longworth <jerrett7@gmail.com>
2023-03-18 21:04:42 -0400

    tests/integration: Add floating-point comparison tests

--- src/tests/integration/core_parser/mod.rs
+++ src/tests/integration/core_parser/mod.rs
@@ -11,6 +11,7 @@ pub mod coprocessor_move;
 pub mod double_arithmetic;
 pub mod double_immediate;
 pub mod floating_point_arithmetic;
+pub mod floating_point_comparison;
 
 #[test]
 fn add_register_plus_itself() -> Result<(), String> {
