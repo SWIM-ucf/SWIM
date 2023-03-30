ad05934deb8cbc1695597116ecb12bdbfa42dfb9
ehraiford <ehraiford@users.noreply.github.com>
2023-03-30 16:17:54 +0000

    deploy: 54d0577f1970e21da228f62e6d6226c4af8c0915

--- swim-cab6fe47534774f7.js
+++ swim-cab6fe47534774f7.js
@@ -1166,7 +1166,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('swim-3ca85e9189f456d5_bg.wasm', import.meta.url);
+        input = new URL('swim-cab6fe47534774f7_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
