f2b9f7e7ae5d2beaf7dd2b6d12a72be7e746651c
Jerrett Longworth <jerrett7@gmail.com>
2023-07-29 16:01:48 -0400

    constants.rs: FUNCT_JR_RELEASE6 -> FUNCT_JR
    This project already assumes the use of MIPS release 6.
    
    Signed-off-by: Jerrett Longworth <jerrett7@gmail.com>

--- src/emulation_core/mips/constants.rs
+++ src/emulation_core/mips/constants.rs
@@ -17,7 +17,7 @@ pub const FUNCT_DSUB: u8 = 0b101110;
 pub const FUNCT_DSUBU: u8 = 0b101111;
 
 pub const FUNCT_JALR: u8 = 0b001001;
-pub const FUNCT_JR_RELEASE6: u8 = FUNCT_JALR;
+pub const FUNCT_JR: u8 = FUNCT_JALR;
 
 /// Used for `MUL` and `MUH`.
 pub const FUNCT_SOP30: u8 = 0b011000;
