984fbf125953228cbe28d398c5d4ca223cbbf66a
Jerrett Longworth <jerrett7@gmail.com>
2023-07-30 19:58:54 -0400

    datapath: Include JALR in JAL specification exception
    Signed-off-by: Jerrett Longworth <jerrett7@gmail.com>

--- src/emulation_core/mips/datapath.rs
+++ src/emulation_core/mips/datapath.rs
@@ -34,9 +34,9 @@
 //!   `bnezalc`, `beqc`, and `bovc` instructions.
 //! - This datapath implements `daddi` as it exists in MIPS64 version 5. This instruction
 //!   was deprecated in MIPS64 version 6.
-//! - Unlike the MIPS64 version 6 specification for the `jal` instruction, `PC + 4` is
-//!   stored in `GPR[31]`, *not* `PC + 8`, as there is no implementation of branch
-//!   delay slots.
+//! - Unlike the MIPS64 version 6 specification for the `jal` and `jalr` instructions,
+//!   `PC + 4` is stored in `GPR[31]`, *not* `PC + 8`, as there is no implementation of
+//!   branch delay slots.
 //!
 //! # Notes on `is_halted`
 //!
