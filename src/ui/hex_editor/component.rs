d1f5d6da9a9f107cff9cfbd16b82a02959f983bf
Cay <cay.henning@proton.me>
2024-01-15 11:15:00 -0500

    start adding hex-editor selection

--- src/ui/hex_editor/component.rs
+++ src/ui/hex_editor/component.rs
@@ -1,7 +1,11 @@
-use yew::{function_component, html, Html, Properties};
+use js_sys::Object;
+use wasm_bindgen::{closure::Closure, JsValue};
+use yew::{function_component, html, Html, Properties, use_callback, Callback, use_mut_ref, use_effect_with_deps};
 use crate::emulation_core::mips::memory::{Memory, MemoryIter};
 use std::rc::Rc;
 use std::cell::RefCell;
+use log::debug;
+use wasm_bindgen::JsCast;
 
 use monaco::{
     api::TextModel,
@@ -19,12 +23,84 @@ pub struct HexEditorProps {
 #[function_component(HexEditor)]
 pub fn hex_editor(props: &HexEditorProps) -> Html {
     let editor_link = CodeEditorLink::new();
+    let text_model = Rc::clone(&props.memory_text_model);
+    let executed_line = js_sys::Array::new();
+    let not_highlighted = js_sys::Array::new();
+    let mut mutated = false;
+
+    use_effect_with_deps(
+        move |_| {
+
+            let memory_text_model = text_model.borrow_mut();
+
+        },
+        executed_line,
+    );
+
+    // create a JavaScript closure
+    let cb = Closure::wrap(Box::new(move |event: monaco::sys::editor::ICursorSelectionChangedEvent| {
+        let highlight_decor = monaco::sys::editor::IModelDecorationOptions::default();
+        highlight_decor.set_class_name("hexHighlight".into());
+
+        debug!("Selection: {:?}", event.selection());
+        let selection = event.selection();
+        let start_line_number = selection.selection_start_line_number();
+        let end_line_number = selection.position_line_number();
+        let start_column = selection.start_column();
+        let end_column = selection.end_column();
+
+        let curr_range = monaco::sys::Range::new(start_line_number, start_column, end_line_number, end_column);
+
+        // element to be stored in the stack to highlight the line
+        let highlight_line: monaco::sys::editor::IModelDeltaDecoration =
+            Object::new().unchecked_into();
+        highlight_line.set_options(&highlight_decor);
+        let range_js = curr_range
+            .dyn_into::<JsValue>()
+            .expect("Hex range is not found.");
+        highlight_line.set_range(&monaco::sys::IRange::from(range_js));
+        let highlight_js = highlight_line
+            .dyn_into::<JsValue>()
+            .expect("Hex highlight is not found.");
+
+        if start_column > 8.0 && end_column < 46.0 {
+            // select ASCII
+        }
+        else if start_column > 45.0 && end_column < 63.0 {
+            // select hex
+        }
+
+
+    }) as Box<dyn FnMut(_)>);
+
+    let on_editor_created = {
+        let text_model = Rc::clone(&props.memory_text_model);
+
+        use_callback(
+            move |editor_link: CodeEditorLink, text_model| {
+                match editor_link.with_editor(|editor| {
+                    let raw_editor = editor.as_ref();
+
+                    debug!("Helo!");
+                    let cb_func = &cb.as_ref().unchecked_ref();
+
+                    raw_editor.on_did_change_cursor_selection(cb_func);
+
+                }) {
+                    Some(()) => debug!("Hex Editor linked!"),
+                    None => debug!("No editor :(")
+                };
+            },
+            text_model,
+        )
+    };
     html! {
         <CodeEditor
             classes={"editor"}
             link={editor_link}
             options={get_options()}
             model={props.memory_text_model.borrow().clone()}
+            on_editor_created={on_editor_created}
         />
     }
 }
