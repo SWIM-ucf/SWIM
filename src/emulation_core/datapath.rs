019d12b1aaa5f9f375544dd2d082d49ae1eb859b
Brooks McKinley <31021010+brooksmckinley@users.noreply.github.com>
2024-02-06 20:39:46 -0500

    Finish up basic commands implementation

--- src/emulation_core/datapath.rs
+++ src/emulation_core/datapath.rs
@@ -25,11 +25,6 @@ pub trait Datapath {
     /// at the discretion of the developer.
     type RegisterEnum;
 
-    /// This enum describes all possible stages in the datapath. This is
-    /// used primarily for the visual datapath view. Must be convertable
-    /// into a string for highlighting purposes.
-    type StageEnum: Into<String>;
-
     /// Execute a single instruction based on the current state of the
     /// datapath. Should the datapath support stages, if the datapath is
     /// midway through a stage, the current instruction will be finished
@@ -48,10 +43,9 @@ pub trait Datapath {
     /// registers should be listed within [`Self::RegisterEnum`].
     fn get_register_by_enum(&self, register: Self::RegisterEnum) -> Self::RegisterData;
 
-    /// Sets the data in the register indicated by the provided enum.
-    fn set_register_by_enum(&self, _register: Self::RegisterEnum, _data: Self::RegisterData) {
-        todo!()
-    }
+    /// Sets the data in the register indicated by the provided string. If it doesn't exist,
+    /// this function returns Err.
+    fn set_register_by_str(&mut self, register: &str, data: Self::RegisterData);
 
     /// Loads the instructions from the provided array into an emulation core's
     /// memory. This will also clear the memory of the emulation core and reset
@@ -74,19 +68,6 @@ pub trait Datapath {
 
     /// Restore the datapath to its default state.
     fn reset(&mut self);
-
-    // Information retrieval
-
-    /// Get the program counter from an emulation core, regardless of what
-    /// it's called.
-    fn get_pc(&self) -> Self::RegisterData {
-        todo!()
-    }
-
-    /// Gets the current stage the emulator core is in.
-    fn get_stage(&self) -> Self::StageEnum {
-        todo!()
-    }
 }
 
 /// A datapath that supports a visual diagram component.
