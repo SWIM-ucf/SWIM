f6d8ee725dd2353fed56b7a82df177fdf0abbf05
Geetis <59862178+Geetis@users.noreply.github.com>
2024-03-13 16:05:27 -0400

    Lowercase assembly example file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -38,7 +38,7 @@ use yew_hooks::prelude::*;
 // To load in the Fibonacci example, uncomment the CONTENT and fib_model lines
 // and comment the code, language, and text_model lines. IMPORTANT:
 // rename fib_model to text_model to have it work.
-const CONTENT: &str = include_str!("../../static/assembly_examples/Fibonacci.asm");
+const CONTENT: &str = include_str!("../../static/assembly_examples/fibonacci.asm");
 const ARCH: AvailableDatapaths = AvailableDatapaths::MIPS;
 
 #[derive(Properties, Clone, PartialEq)]
