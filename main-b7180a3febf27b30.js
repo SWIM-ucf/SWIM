b33dcaf4307fd6accbf1ec3d824a817451a19c1e
cayb0rg <cayb0rg@users.noreply.github.com>
2024-04-20 01:13:50 +0000

    deploy: d7511a1dc3d90f24bb40dbc587fe084b96d3f97a

--- main-b7180a3febf27b30.js
+++ main-b7180a3febf27b30.js
@@ -1346,7 +1346,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('main-f6dca689917cc4d8_bg.wasm', import.meta.url);
+        input = new URL('main-b7180a3febf27b30_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
